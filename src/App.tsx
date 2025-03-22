f1cc8c74eaff6e29c9076c40776eaddda788c9d9
Peter Sterner <psterner@gmail.com>
2025-03-22 13:54:04 +0100

    Omstrukturering.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import { Routes, Route, Outlet } from "react-router";
 
-const Home = () => <h1>Hjem</h1>;
-const About = () => <h1>Om os</h1>;
-
-const Dashboard = () => (
-  <div>
-    <h1>Dashboard</h1>
-    <Outlet /> {/* Under-ruter vises her */}
-  </div>
+const NavBar = () => (
+  <nav className="fixed top-0 left-0 w-full bg-blue-600 text-white shadow-md">
+    <Brand></Brand>
+    <KnapperSignedIn></KnapperSignedIn>
+  </nav>
 );
 
-const DashboardHome = () => <h2>Velkommen til Dashboard</h2>;
-const Stats = () => <h2>Statistik</h2>;
+const Brand = () => (
+  <div className="container mx-auto flex justify-between items-center p-4">
+    <a href="#" className="text-white text-lg font-bold">
+      Brand
+    </a>
+  </div>
+);
+const KnapperSignedIn = () => (
+  <ul className="flex flex-row space-x-4">
+    <li>
+      <a href="/forside" className="text-gray-300 hover:text-white">
+        Forside
+      </a>
+    </li>
+    <li>
+      <a href="om" className="text-gray-300 hover:text-white">
+        Om
+      </a>
+    </li>
+  </ul>
+);
+const Hjem = () => (
+  <>
+    <NavBar></NavBar>
+    <Outlet></Outlet>
+  </>
+);
+const Om = () => <h1>Om os</h1>;
+const Forside = () => <h1>Forside</h1>;
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="dashboard" element={<Dashboard />}>
-        <Route index element={<DashboardHome />} /> {/* Standardvisning */}
-        <Route path="stats" element={<Stats />} />
+      <Route path="/" element={<Hjem />}>
+        <Route index element={<Forside />} /> {/* Standardvisning */}
+        <Route path="om" element={<Om />} />
       </Route>
     </Routes>
   );
