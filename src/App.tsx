c1a183fd56dda5ab46242ce2cb66ef6c9c3e7295
petey33 <psterner@gmail.com>
2025-03-22 11:55:16 +0100

    add routing with react-router and restructure App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import "./App.css";
+import { Routes, Route, Outlet } from "react-router";
+
+const Home = () => <h1>Hjem</h1>;
+const About = () => <h1>Om os</h1>;
+
+const Dashboard = () => (
+  <div>
+    <h1>Dashboard</h1>
+    <Outlet /> {/* Under-ruter vises her */}
+  </div>
+);
+
+const DashboardHome = () => <h2>Velkommen til Dashboard</h2>;
+const Stats = () => <h2>Statistik</h2>;
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="dashboard" element={<Dashboard />}>
+        <Route index element={<DashboardHome />} /> {/* Standardvisning */}
+        <Route path="stats" element={<Stats />} />
+      </Route>
+    </Routes>
   );
 }
 
