82e7f374dcb6a9ef2e0a465808a8a87fd8cdc740
petey33 <psterner@gmail.com>
2025-03-24 19:02:00 +0100

    add SignInPage component and update routing for login

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,55 @@ function App() {
           Om
         </Link>
       </li>
+      <li>
+        <Link to="logind" className="text-gray-300 hover:text-white">
+          Log ind
+        </Link>
+      </li>
     </ul>
+  );
+
+  const SignInPage = () => (
+    <form>
+      <div className="flex flex-col space-y-4">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
+    </form>
   );
 
   const SignedInButtons = () => (
@@ -62,11 +110,9 @@ function App() {
     <>
       <div className="flex flex-col h-screen">
         <nav className="bg-gray-800 p-4">
-          Navbar:
           <NavBar></NavBar>
         </nav>
         <div className="flex-grow p-4 v-screen">
-          Indhold:
           <Outlet></Outlet>
         </div>
       </div>
@@ -84,6 +130,7 @@ function App() {
       <Route path="/" element={<Design />}>
         <Route path="" index element={<Forside />} />
         <Route path="forside" element={<Forside />} />
+        <Route path="logind" element={<SignInPage />} />
         {/* Standardvisning */}
         <Route path="om" element={<Om />} />
         <Route path="indstillinger" element={<Indstillinger />} />
