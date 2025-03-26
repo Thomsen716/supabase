c00b61d3fc8742b7e2858085d5cea8556b2d9efa
petey33 <psterner@gmail.com>
2025-03-26 18:59:57 +0100

    refactor Brand component to remove unnecessary padding from container

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function NavBar(props: {
 
 function Brand() {
   return (
-    <div className="container mx-auto flex justify-between items-center p-4">
+    <div className="container mx-auto flex justify-between items-center">
       <Link to="forside" className="text-white text-lg font-bold">
         Brand
       </Link>
