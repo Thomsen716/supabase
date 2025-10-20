e1a9ec39a9cdd49a76d344c5d0cec220c3d21c5c
Peter Sterner <psterner@gmail.com>
2025-10-20 09:59:29 +0200

    .

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -183,20 +183,15 @@ const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     firstName: string,
     lastName: string
   ) => {
-    const session = await supabase.auth.getSession();
-    console.log("auth.uid():", session.data.session?.user.id);
-    console.log("user?.id:", user?.id);
     const { error } = await supabase
       .from("users")
-      .update(
-        {
-          id: user?.id,
-          first_name: firstName,
-          last_name: lastName,
-        }
-        //{ onConflict: "user_id" } // Sikrer, at den kun opdaterer, hvis ID allerede findes
-      )
+      .update({
+        id: user?.id,
+        first_name: firstName,
+        last_name: lastName,
+      })
       .eq("id", user?.id);
+
     if (error) {
       console.error(error);
       return { error };
